3f6274c91581538f46c95c9e10c0194ad97b2317
Mahir-Neema <mohi10503@gmail.com>
2023-04-10 23:36:43 +0530

    added question_reducer

--- quizApp/src/redux/question_reducer.js
+++ quizApp/src/redux/question_reducer.js
@@ -1,3 +1,22 @@
-export const questionReducer = () => {
-    
-}
+import { createSlice } from "@reduxjs/toolkit"
+
+// reducer with initial value
+export const questionReducer = createSlice({
+    name: 'questions',
+    initialState:{
+        queue:[],
+        answers:[],
+        trace: 0
+    },
+    reducers:{
+        startQuizAction:(state,action)=>{
+            return {
+                ...state,
+                queue: action.payload
+            }
+        }
+    }
+})
+
+export const {startQuizAction} = questionReducer.actions;
+export default questionReducer.reducer;
--- quizApp/src/redux/store.js
+++ quizApp/src/redux/store.js
@@ -2,12 +2,12 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 
 // calling reducer 
-import { questionReducer } from "./question_reducer";
+import questionReducer from "./question_reducer";
 import { resultReducer } from "./result_reducer";
 const rootReducer = combineReducers({
     questions:questionReducer,
     result: resultReducer
 })
 
-// create store using reducer 
+// creating store using reducer 
 export default configureStore({reducer: rootReducer})
